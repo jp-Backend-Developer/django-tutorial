55cfb6bbad1d2f55feee770ec6eead57ca2666b7
taishi-yamada <taishi.37.0327@gmail.com>
2022-12-24 23:08:17 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from http.client import HTTPResponse
+from pyexpat import model
+from django.shortcuts import render
+from .models import Post
+
+def frontpage(request):
+    posts = Post.objects.all()
+    return render(request, "blog/frontpage.html", {"posts":posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "blog/post_detail.html", {"post":post})
