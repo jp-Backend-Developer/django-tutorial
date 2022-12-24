0cdd492a6bf3bd8e92139bcd670bec7c5ce42a48
taishi-yamada <taishi.37.0327@gmail.com>
2022-12-24 23:41:10 +0900

    from .local_settings import SECRET_KEY

---
+++ microblog/local.settings.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-efgekn&yl5oz+ak6+%69$$x88!d_hi7#-ntef6-^2(em0rz^5w'
--- microblog/settings.py
+++ microblog/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-efgekn&yl5oz+ak6+%69$$x88!d_hi7#-ntef6-^2(em0rz^5w'
-
+from .local_settings import SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
